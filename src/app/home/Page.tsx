1686762e7d0045381063205140713f15de17f145
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-08 16:27:10 +0545

    Updated

--- src/app/home/Page.tsx
+++ src/app/home/Page.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const Page = () => {
   return (
-    <div className="px-25 py-10">
-     
-      <div className="flex flex-col items-center justify-center h-100">
+    <div className="px-25 py-10 ">
+      <div className="flex flex-col items-center justify-center h-125 gap-3">
         {/* Outer div with gradient background acting as border */}
         <div className="p-2 bg-gradient-to-r from-indigo-500 to-purple-600 rounded-full">
           {/* Inner Image with solid background */}
@@ -18,10 +17,21 @@ const Page = () => {
           />
         </div>
 
-        <h1 className="font-bold text-white text-8xl">Hi, I'm Prabhat</h1>
-        <h1 className="font-bold text-white text-7xl">Designer and Developer</h1>
+        <h1 className="font-bold text-white text-8xl">Hi. I`m Prabhat.</h1>
+        <h1 className="font-bold text-white text-6xl">
+          Designer. || Developer.
+        </h1>
+        <p className="text-white font-semibold">
+          I`m passionate about crafting experiences that are engaging,
+          accessible, and user-centric.
+        </p>
       </div>
-      <hr className="h-0.5 bg-blue-800 w-full" />
+
+      <div id="skills" className="h-150 border-blue-800 border-2 rounded-2xl mt-12">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-white text-4xl font-semibold py-10">Skills</h1>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   const links = [
     { name: 'Home', href: '/' },
     { name: 'About', href: '/about' },
     { name: 'Contact', href: '/contact' },
-    {name: 'Blog', href: '/blog'},
-    {name:'Projects', href: '/projects'},
+    { name: 'Blog', href: '/blog' },
+    { name: 'Projects', href: '/projects' },
+  ];
 
-  ]
   return (
     <div className="px-25 py-12 flex justify-center items-center">
-      <nav className="flex fixed  justify-center items-center p-4 border-3 p-1 border-white transition ease-in-out duration 500 rounded-4xl text-white">
-
-        <ul className="flex space-x-4 font-black">
-          {links.map((link) => (
-            <li key={link.name}>
-              <a href={link.href} className="hover:text-gray-400 transition ease-in-out duratio-500 ">{link.name}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      {/* Outer div with gradient background acting as border */}
+      <div className="fixed p-1 bg-gradient-to-r from-indigo-500 to-purple-600 rounded-4xl">
+        {/* Inner nav with solid background */}
+        <nav className="flex justify-center items-center p-4 bg-black rounded-4xl text-white transition ease-in-out duration-500">
+          <ul className="flex space-x-4 font-black">
+            {links.map((link) => (
+              <li key={link.name}>
+                <a
+                  href={link.href}
+                  className="hover:text-gray-400 transition ease-in-out duration-500"
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
