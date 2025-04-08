a863a680c9074d75ab75710e147b77ac7fbb39a5
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-08 13:15:15 +0545

    Modified files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1 className="font-bold text-4xl text-red-900">Hello World</h1>
+      
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { RiSunLine,RiMoonLine } from '@remixicon/react'
 const Navbar = () => {
+  const links = [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '/about' },
+    { name: 'Contact', href: '/contact' },
+    {name: 'Blog', href: '/blog'},
+    {name:'Projects', href: '/projects'},
+    {name:<RiSunLine/>, href: '/sun'},
+    {name:<RiMoonLine/>, href: '/moon'},
+  ]
   return (
-    <div>
-      
+    <div className="">
+      <nav className="flex fixed w-full justify-between items-center p-4 border-1 border-gray-800 rounded text-black">
+        <div className="text-2xl font-bold ">.Porfolio </div>
+        <ul className="flex space-x-4 font-semibold">
+          {links.map((link) => (
+            <li key={link.name}>
+              <a href={link.href} className="hover:text-gray-400">{link.name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   )
 }
